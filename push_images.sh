5f89f9f3a5e01999bc7aeaa6d3b4b5f0276624a4
Adrian Grzemski <Wmj3dp#w>
2019-03-25 15:57:11 +0100

    Changed names of docker push script

--- push_images.sh
+++ push_images.sh
@@ -3,7 +3,8 @@
 set -eux
 
 IMAGE_NAME="grzadr/biosak"
-DATE_TAG=$(date '+%y-%m-%d')
+#DATE_TAG=$(date '+%y-%m-%d')
+DATE_TAG="{1}"
 
 docker push "${IMAGE_NAME}":latest
 docker push "${IMAGE_NAME}":"${DATE_TAG}"
